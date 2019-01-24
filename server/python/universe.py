369ce429352a6b76204f5f4dcac55fabef52789e
Jeremy McGibbon <mcgibbon@atmos.washington.edu>
2019-01-23 22:11:34 -0800

    Removed empty file

--- server/python/universe.py
+++
@@ -1,9 +0,0 @@
-import cachetools
-from esi import get_op
-import json
-import os
-from database import (
-    Type, Corporation, System, Alliance, TypePrice, Region, Group, Station,
-    Structure, Constellation, get_corporation, get_alliance
-)
-
