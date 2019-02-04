62ec16bbd0a4acb86ff0b303f0ab75fa31d7a5a2
Jeremy McGibbon <mcgibbon@uw.edu>
2019-02-03 18:45:38 -0800

    Began template unit tests

--- server/admin.py
+++ server/admin.py
@@ -1,17 +1,2 @@
 from models import User, Character
 
-
-def get_user_list():
-    # list of all Users with roles
-    result = {}
-    for user in User.query().run():
-        id = user.get_id()
-        user_name = Character.get(id).name if id else None
-        result[id] = {
-            'user_id': id,
-            'is_recruiter': user.is_recruiter,
-            'is_snr_recruiter': user.is_senior_recruiter,
-            'is_admin': user.is_admin,
-            'name': user_name,
-        }
-    return result
