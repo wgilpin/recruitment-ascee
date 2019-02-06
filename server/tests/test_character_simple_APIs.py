b0b18b44ae0fda3159dcbbab3c5ec834b3977826
William Gilpin <wgilpin@gmail.com>
2019-02-06 18:25:37 +0000

    removed debug line

--- server/tests/test_character_simple_APIs.py
+++ server/tests/test_character_simple_APIs.py
@@ -93,7 +93,6 @@ class SimpleCharTests(AsceeTestCase):
     def helper_simple_APIs(self, response, api_def):
         self.assertIn('info', response)
         for key, entry in response['info'].items():
-            print('XXXXXXXX', api_def['required'])
             for property_name, property_type in api_def['required'].items():
                 self.assertIn(property_name, entry)
                 self.assertIsInstance(entry[property_name], property_type)
