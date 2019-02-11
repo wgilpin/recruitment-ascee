dad8f93a5e3a8e8689da9966df06ec169de5b5c5
William Gilpin <wgilpin@gmail.com>
2019-02-11 12:47:04 +0000

    added FE for some evidence APIs

---
+++ client/src/TableIndustry.js
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import TableBase from './TableBase';
+
+const propTypes = {
+  alt: PropTypes.string,
+};
+
+const defaultProps = {};
+
+export default class TableIndustry extends TableBase {
+  constructor(props) {
+    super(props);
+    this.state.sortBy = 'name';
+    this.scope = 'planetary_interaction';
+    this.addField(TableBase.kinds().text, 'blueprint_type_name', 'Type');
+    this.addField(TableBase.kinds().number, 'blueprint_type_id');
+    this.addField(TableBase.kinds().number, 'cost');
+    this.addField(TableBase.kinds().number, 'duration');
+    this.addField(TableBase.kinds().date,'end_date');
+    this.addField(TableBase.kinds().number, 'licensed_runs');
+    this.addField(TableBase.kinds().number, 'output_location_id');
+    this.addField(TableBase.kinds().text, 'output_location_name');
+    this.addField(TableBase.kinds().number, 'runs');
+    this.addField(TableBase.kinds().date, 'start_date');
+    this.addField(TableBase.kinds().number, 'station_id');
+    this.addField(TableBase.kinds().text, 'station_name');
+    this.addField(TableBase.kinds().text, 'status');
+  }
+}
+
+TableBase.propTypes = propTypes;
+TableBase.defaultProps = defaultProps;
---
+++ client/src/TablePI.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import TableBase from './TableBase';
+
+const propTypes = {
+  alt: PropTypes.string,
+};
+
+const defaultProps = {};
+
+export default class TablePI extends TableBase {
+  constructor(props) {
+    super(props);
+    this.state.sortBy = 'name';
+    this.groupBy = ['region']
+    this.scope = 'planetary_interaction';
+    this.addField(TableBase.kinds().date,'last_update');
+    this.addField(TableBase.kinds().number, 'num_pins', '# Pins');
+    this.addField(TableBase.kinds().text, 'planet_type');
+    this.addField(TableBase.kinds().text, 'solar_system_name');
+    this.addField(TableBase.kinds().text, 'region_name');
+    this.addField(TableBase.kinds().number, 'region_id');
+    this.addField(TableBase.kinds().number, 'solar_system_id');
+    this.addField(TableBase.kinds().number, 'upgrade_level');
+  }
+}
+
+TableBase.propTypes = propTypes;
+TableBase.defaultProps = defaultProps;
