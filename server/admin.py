16965c419a6c156f44c0fdbef1ff1403df02797f
William Gilpin <wgilpin@gmail.com>
2019-02-11 16:30:16 +0000

    added admin list tests

--- server/admin.py
+++ server/admin.py
@@ -1,5 +1,5 @@
 from models import db
-from models.Admin import List, ListItem
+from models import List, ListItem, list_kinds
 from flask import jsonify
 from flask_app import app
 from flask_login import login_required, current_user
@@ -9,7 +9,6 @@ from datetime import date, datetime
 from json import dumps
 
 SECONDS_TO_CACHE = 10 * 60
-
 
 @app.route('/api/admin/list/<string:kind>', methods=['GET'])
 @login_required
@@ -34,6 +33,11 @@ def api_admin_list(kind):
 
 def get_admin_list(kind, current_user=None):
     user_admin_access_check(current_user)
-    response = List.get_by_kind(kind)
+    if not kind in list_kinds:
+        raise BadRequestException('Unknown Redlist')
+    response = []
+    the_list = List.get_by_kind(kind)
+    if the_list:
+        for item in the_list.items:
+            response.append({ 'id': item.id, 'name': item.name })
     return {'info': response}
-
--- server/models/admin.py
+++ server/models/admin.py
@@ -1,6 +1,16 @@
 from models.database import db
+import datetime
 
-class List(db.model):
+list_kinds = [
+    'character',
+    'type',
+    'channel',
+    'alliance',
+    'corporation',
+    'system'
+    ]
+
+class List(db.Model):
     __tablename__ = 'admin_list'
     id = db.Column(db.Integer, primary_key=True)
     kind = db.Column(db.String(10))
@@ -8,10 +18,31 @@ class List(db.model):
 
     @classmethod
     def get_by_kind(cls, kind):
-        return  db.session.query(cls).filter_by(kind=kind).one()
+        return db.session.query(cls).filter_by(kind=kind).one_or_none()
 
 class ListItem(db.Model):
     __tablename__ = 'admin_list_item'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(10))
+    list_id = db.Column(db.Integer, db.ForeignKey(List.id))
     list = db.relationship("List", uselist=False, back_populates="items")
+
+list_cache = {}
+list_cache_time = datetime.datetime(year=1970, month=1, day=1)
+LIST_CACHE_TTL = datetime.timedelta(minutes=10)
+
+def get_redlists():
+    lists = {}
+    for kind in list_kinds:
+        lists[kind] = set()
+        this_list = List.get_by_kind(kind)
+        if this_list:
+            for item in List.get_by_kind(kind).items:
+                lists[kind].add(item.id)
+    return lists
+
+def check_redlist(esi_id, kind):
+    global list_cache
+    if datetime.datetime.now() > list_cache_time + LIST_CACHE_TTL:
+        list_cache = get_redlists()
+    return esi_id in list_cache[kind]
