539c218896f5f0c8ee815a58baecaf71be67f421
Jeremy McGibbon <mcgibbon@uw.edu>
2019-02-14 13:01:12 -0800

    implemented industry and PI endpoints

--- server/character/planetary_interaction.py
+++ server/character/planetary_interaction.py
@@ -1,3 +1,22 @@
+from models import Character, System
+from security import character_application_access_check
+
 
 def get_character_planetary_interaction(character_id, current_user=None):
-    raise NotImplementedError()
+    character = Character.get(character_id)
+    character_application_access_check(current_user, character)
+    planet_data = character.get_op(
+        'get_characters_character_id_planets',
+        character_id=character_id,
+    )
+    system_ids = set()
+    for entry in planet_data:
+        system_ids.add(entry['solar_system_id'])
+    system_dict = System.get_multi(list(system_ids))
+
+    for entry in planet_data:
+        system = system_dict[entry['solar_system_id']]
+        entry['system_name'] = system.name
+        entry['region_id'] = system.region_id
+        entry['region_name'] = system.region.name
+    return {'info': planet_data}
