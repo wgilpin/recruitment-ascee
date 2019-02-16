a6a38028dda9b1d901a82a496c1297e5063b8e15
William Gilpin <wgilpin@gmail.com>
2019-02-16 16:24:36 +0000

    loaded industry api for use

--- server/models/eve.py
+++ server/models/eve.py
@@ -423,7 +423,7 @@ class Structure(db.Model):
         if self.redlisted:
             return True
         else:
-            return self.system.is_redlisted
+            return not self.system or self.system.is_redlisted
 
 
 class Character(db.Model):
