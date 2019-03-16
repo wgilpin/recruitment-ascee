a8cdd213dc81fc4d7873c5257da00a595c54270d
William Gilpin <wgilpin@gmail.com>
2019-03-16 17:49:57 +0000

    heroku issues

--- server/main.py
+++ server/main.py
@@ -53,7 +53,7 @@ def api_server_error(e):
 
 def run_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.json_encoder = CustomJSONEncoder
     with app.app_context():
         db.init_app(app)
