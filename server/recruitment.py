73f4e3f1895851e3d190d19883c9224d4efcf5e2
Jeremy McGibbon <mcgibbon@uw.edu>
2019-03-23 23:13:27 -0700

    Reimplemented submit_application

--- server/recruitment.py
+++ server/recruitment.py
@@ -34,13 +34,11 @@ def remove_question(question_id, current_user=None):
     return {'status': 'ok'}
 
 
-def submit_application(data, current_user=None):
-    if is_admin(current_user) or is_recruiter(current_user) or is_senior_recruiter(current_user):
-        raise BadRequestException(f'User {current_user.id} is not an applicant')
+def submit_application(current_user=None):
     application = Application.get_for_user(current_user.id)
     if not application:
-        application = Application(user_id=current_user.id)
-        db.session.add(application)
+        raise BadRequestException(f'User {current_user.id} is not an applicant.')
+    application.is_submitted = True
     db.session.commit()
     return {'status': 'ok'}
 
