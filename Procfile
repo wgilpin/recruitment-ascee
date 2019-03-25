c023e0f5460e9ce0668414d897419b0983aa3c82
William Gilpin <wgilpin@gmail.com>
2019-03-25 22:10:00 +0000

    more heroku startup

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --chdir ./server server.wsgi:run_app
+web: gunicorn --chdir ./server server.wsgi:app
--- server/wsgi.py
+++ server/wsgi.py
@@ -63,9 +63,7 @@ def run_app():
     with app.app_context():
         db.init_app(app)
         db.create_all()
-        if os.environ.get('CURRENT_ENV', '') == 'heroku':
-            app.run()
-        else:
+        if os.environ.get('CURRENT_ENV', '') != 'heroku':
             app.run(host='localhost', port='8080')
 
 run_app()
