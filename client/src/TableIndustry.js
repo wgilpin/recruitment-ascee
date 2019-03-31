fcf04283f1fa83d8efe18c17798f1a23542bff46
William Gilpin <wgilpin@gmail.com>
2019-03-31 12:14:42 +0100

    reordered industry UI columns

--- client/src/TableIndustry.js
+++ client/src/TableIndustry.js
@@ -10,18 +10,18 @@ const defaultProps = {};
 export default class TableIndustry extends TableBase {
   constructor(props) {
     super(props);
-    this.state.sortBy = 'name';
+    this.state.sortBy = 'end_date';
     this.scope = 'industry';
     this.addField(TableBase.kinds().text, 'blueprint_type_name', 'Type');
-    this.addField(TableBase.kinds().number, 'cost');
-    this.addField(TableBase.kinds().number, 'duration');
-    this.addField(TableBase.kinds().date,'end_date');
+    this.addField(TableBase.kinds().ISK, 'cost');
     this.addField(TableBase.kinds().number, 'licensed_runs');
-    this.addField(TableBase.kinds().text, 'output_location_name');
     this.addField(TableBase.kinds().number, 'runs');
     this.addField(TableBase.kinds().date, 'start_date');
+    this.addField(TableBase.kinds().date,'end_date');
     this.addField(TableBase.kinds().text, 'station_name');
     this.addField(TableBase.kinds().text, 'status');
+    this.addField(TableBase.kinds().number, 'duration');
+    this.addField(TableBase.kinds().text, 'output_location_name');
   }
 }
 
