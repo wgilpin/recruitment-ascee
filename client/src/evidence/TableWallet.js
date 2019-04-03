c89c25df521e83440904c40a9482852d69fbe876
William Gilpin <wgilpin@MacBook-2.local>
2019-04-03 22:19:08 +0100

    display divs from corp wallets

--- client/src/evidence/TableWallet.js
+++ client/src/evidence/TableWallet.js
@@ -1,22 +1,79 @@
-import PropTypes from 'prop-types';
-import TableBase from './TableBase';
+import React from "react";
+import PropTypes from "prop-types";
+import TableBase from "./TableBase";
 
 const propTypes = {
-  alt: PropTypes.string,
+  alt: PropTypes.string
 };
 
 const defaultProps = {};
 
+const styles = {
+  select: {
+    padding: "8px",
+    margin: "8px",
+    width: "300px",
+    backgroundColor: "#111",
+    color: "white",
+    height: "24px",
+    borderWidth: "1px",
+    borderColor: "grey"
+  }
+};
 export default class TableWallet extends TableBase {
   constructor(props) {
     super(props);
-    this.state.sortBy = 'date';
-    this.scope = 'wallet';
-    this.addField(TableBase.kinds().date,'date');
-    this.addField(TableBase.kinds().ISK,'amount');
-    this.addField(TableBase.kinds().ISK,'balance');
-    this.addField(TableBase.kinds().text,'other_party');
-    this.addField(TableBase.kinds().text,'description');
+    this.state.sortBy = "date";
+    this.scope = "wallet";
+    this.addField(TableBase.kinds().date, "date");
+    this.addField(TableBase.kinds().ISK, "amount");
+    this.addField(TableBase.kinds().ISK, "balance");
+    this.addField(TableBase.kinds().text, "other_party");
+    this.addField(TableBase.kinds().text, "description");
+  }
+
+  handleSelect = division => {
+    this.setState({ division: division.target.value }, 
+      () => this.processData(this.preProcessData(this.state.rawData))
+    );
+  };
+
+  preProcessData(data) {
+    if (this.state.rawData[0].division_name) {
+      if (!this.state.division) {
+        console.log("no state div");
+        console.log("set division", this.state.rawData[0].division_name);
+        this.setState({ division: this.state.rawData[0].division_name });
+        return this.state.rawData[0];
+      }
+      const the_div = data.filter(
+        div => div.division_name === this.state.division
+      )[0];
+      if (the_div) {
+        console.log("return the_div");
+        return the_div;
+      }
+    }
+    console.log("no divisions, return data");
+    return data;
+  }
+
+  showHeader(data) {
+    if (data.length && data[0].division_name) {
+      return (
+        <div>
+          <select style={styles.select} onChange={this.handleSelect}>
+            {data.map(division => (
+              <option value={division.division_name}>
+                {division.division_name}
+              </option>
+            ))}
+          </select>
+        </div>
+      );
+    } else {
+      return null;
+    }
   }
 }
 
