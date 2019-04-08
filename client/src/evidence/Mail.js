55f26f89a2de756a0abe915e110fa850e4782332
William Gilpin <wgilpin@gmail.com>
2019-04-08 21:06:10 +0100

    check who mail is from

--- client/src/evidence/Mail.js
+++ client/src/evidence/Mail.js
@@ -13,8 +13,7 @@ const propTypes = {
   mailList: PropTypes.array,
 };
 
-const defaultProps = {
-};
+const defaultProps = {};
 
 const styles = {
   ...TableStyles.styles,
@@ -39,8 +38,8 @@ const styles = {
   iconBtn: {
     width: '60px',
     float: 'left',
-  }
-}
+  },
+};
 
 export default class Mail extends React.Component {
   constructor(props) {
@@ -67,28 +66,31 @@ export default class Mail extends React.Component {
     let newList;
 
     // get the mail headers by API
-    new FetchData(
-      { id: this.props.targetId, scope: 'character', param1: 'mail' },
-    ).get(this.state.lastMailId ? { last_mail_id: this.state.lastMailId} : {})
+    new FetchData({
+      id: this.props.targetId,
+      scope: 'character',
+      param1: 'mail',
+    })
+      .get(this.state.lastMailId ? { last_mail_id: this.state.lastMailId } : {})
       .then(data => {
         // got the list of mail headers
         newList = Mail.jsonToMailList(data);
-        this.setState({ lastMailId: newList[newList.length-1].mail_id });
-        if (! this.state.endPageOneId) {
-          this.setState({ endPageOneId: newList[newList.length-1].mail_id });
+        this.setState({ lastMailId: newList[newList.length - 1].mail_id });
+        if (!this.state.endPageOneId) {
+          this.setState({ endPageOneId: newList[newList.length - 1].mail_id });
         }
       })
       .then(() => {
         const sortedMailList = newList
           .map(item => ({ ...item, collapsed: true }))
-          .sort((a,b) => (new Date(b.timestamp) - new Date(a.timestamp)))
-        this.setState({ mailList: sortedMailList, loading: false })
+          .sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));
+        this.setState({ mailList: sortedMailList, loading: false });
       })
-      .catch((err) => {
+      .catch(err => {
         console.error(err);
-        this.setState({ loading: false })
-      })
-  }
+        this.setState({ loading: false });
+      });
+  };
 
   badlyRemoveFontSizeColor(html) {
     let small = html.replace(/<font size=['"]\d*['"]/g, '<font size="unset"');
@@ -102,43 +104,49 @@ export default class Mail extends React.Component {
      *  These are of the form showinfo:<typeId>//<itemId>
      *  Then fetch the text from the API for each of them
      */
-    let regexp = /showinfo:(\d+)\/\/(\d+)">([\w\s]+)</g
+    let regexp = /showinfo:(\d+)\/\/(\d+)">([\w\s]+)</g;
     let matches;
     // eslint-disable-next-line no-cond-assign
-    while(matches = regexp.exec(body)){
+    while ((matches = regexp.exec(body))) {
       linksList.push({ typeId: matches[1], itemId: matches[2] });
     }
-    if (linksList.length === 0){
+    if (linksList.length === 0) {
       return null;
     }
     let encodedList = encodeURIComponent(JSON.stringify(linksList));
-    return new FetchData({ scope: 'link', id: this.props.targetId, param1: encodedList }).get();
+    return new FetchData({
+      scope: 'link',
+      id: this.props.targetId,
+      param1: encodedList,
+    }).get();
   }
 
-  processLinks(links, body){
+  processLinks(links, body) {
     /* Given a list of text values for links, from this.findLinks(),
      *  replace the links with the text
      */
-    if (!links){
+    if (!links) {
       return body;
-    };
+    }
     let markup;
     links.forEach(link => {
       let lookupRegex = new RegExp(`${link.itemId}">([\\w\\s]+)<`, 'g');
-      if ('type' in (link || {})){
-        if (link.type === 'character'){
+      if ('type' in (link || {})) {
+        if (link.type === 'character') {
           markup = `${link.data.corporation_ticker}`;
-          markup += link.data.alliance_ticker ? ` / ${link.data.alliance_ticker}` : '';
+          markup += link.data.alliance_ticker
+            ? ` / ${link.data.alliance_ticker}`
+            : '';
         }
       } else {
         markup = link.data.system_name;
-      };
+      }
       body = body.replace(lookupRegex, `${link.itemId}">$1 [${markup}]<`);
     });
     return body;
   }
 
-  toggleMessage = (idx) => {
+  toggleMessage = idx => {
     // toggle message body visibility, loading if needed
     let linksList = [];
     let { mailList } = this.state;
@@ -148,86 +156,92 @@ export default class Mail extends React.Component {
     updatedMailList[idx] = { ...thisMail, collapsed: !thisMail.collapsed };
     this.setState({ mailList: updatedMailList });
     if (!thisMail.body) {
-      return new FetchData(
-        { id: this.props.targetId, scope: 'character', param1: 'mail', param2: thisMail.mail_id },
-      ).get()
-        .then((body) => {
+      return new FetchData({
+        id: this.props.targetId,
+        scope: 'character',
+        param1: 'mail',
+        param2: thisMail.mail_id,
+      })
+        .get()
+        .then(body => {
           rawBody = this.badlyRemoveFontSizeColor(body);
-          this.findLinks(rawBody, linksList)
+          this.findLinks(rawBody, linksList);
         })
         .then(links => this.processLinks(links, rawBody))
-        .then((body) => {
+        .then(body => {
           const updatedMailList = mailList.slice(0);
           updatedMailList[idx] = { ...thisMail, collapsed: false, body };
           this.setState({ mailList: updatedMailList });
-        })
+        });
     }
   };
 
-
-  mailItem(key, { timestamp, from_name, subject, is_read, redlisted }) {
+  mailItem(
+    key,
+    { timestamp, from, from_name, recipients, subject, is_read, redlisted }
+  ) {
     // a react item
     let lineStyle, formattedDate;
     let readStyle = is_read ? styles.isRead : styles.isUnread;
 
-    lineStyle = (key % 2 === 0 ? styles.isOdd : {});
+    lineStyle = key % 2 === 0 ? styles.isOdd : {};
     lineStyle = { ...lineStyle, ...readStyle, ...styles.cell };
-    formattedDate = moment(timestamp).format('DD-MMM-YYYY HH:MM')
-    let nameStyle = {...lineStyle};
+    formattedDate = moment(timestamp).format('DD-MMM-YYYY HH:MM');
+    let nameStyle = { ...lineStyle };
+    const { recipient_name, recipient_id } = recipients[0];
+    let otherParty = from_name;
+    if (this.props.targetId === from.toString()) {
+      otherParty = `TO: ${recipient_name}`;
+    }
     if (redlisted.indexOf('from_name') > -1) {
       nameStyle = { ...lineStyle, ...styles.redList };
     }
     return (
       <div key={key} style={styles.row} onClick={() => this.toggleMessage(key)}>
         <div style={lineStyle}>{formattedDate}</div>
-        <div style={nameStyle}>{from_name}</div>
+        <div style={nameStyle}>{otherParty}</div>
         <div style={lineStyle}>{subject}</div>
       </div>
     );
   }
 
-  mailBody = (line) => {
+  mailBody = line => {
     return (
-      !this.state.mailList[line].collapsed &&
-      (<div style={styles.row}>
-        <div
-          style={styles.body }
-          dangerouslySetInnerHTML={{ __html: this.state.mailList[line].body }}
-        />
-      </div>
+      !this.state.mailList[line].collapsed && (
+        <div style={styles.row}>
+          <div
+            style={styles.body}
+            dangerouslySetInnerHTML={{ __html: this.state.mailList[line].body }}
+          />
+        </div>
       )
-    )
-  }
+    );
+  };
 
   showFirst = () => {
-    this.setState({
-      lastMailId: null,
-      loading: true,
-    },
-      this.componentDidMount,
-    )
-  }
+    this.setState(
+      {
+        lastMailId: null,
+        loading: true,
+      },
+      this.componentDidMount
+    );
+  };
 
   showMore = () => this.setState({ loading: true }, this.componentDidMount);
 
   render() {
     if (this.state.loading) {
-      return(
-      <Loader
-        type="Puff"
-        color="#01799A"
-        height="100"
-        width="100"
-      />)
+      return <Loader type="Puff" color="#01799A" height="100" width="100" />;
     }
-    if (this.state.mailList.length === 0){
-      return <div>No Mail</div>
+    if (this.state.mailList.length === 0) {
+      return <div>No Mail</div>;
     }
     return (
       <div style={styles.table}>
         <div style={styles.header}>
           <div style={styles.cell}>DATE</div>
-          <div style={styles.cell}>FROM</div>
+          <div style={styles.cell}>WITH</div>
           <div style={styles.cell}>SUBJECT</div>
         </div>
         {Object.keys(this.state.mailList).map((line, idx) => {
@@ -237,10 +251,9 @@ export default class Mail extends React.Component {
               {this.mailBody(line)}
             </React.Fragment>
           );
-        })
-        }
+        })}
         <div>
-          {this.state.lastMailId !== this.state.endPageOneId &&
+          {this.state.lastMailId !== this.state.endPageOneId && (
             <IconBtn
               src={FirstPageImg}
               alt="first"
@@ -248,7 +261,7 @@ export default class Mail extends React.Component {
               label="First"
               style={styles.iconBtn}
             />
-          }
+          )}
           <IconBtn
             src={NextPageImg}
             alt="next"
@@ -258,7 +271,7 @@ export default class Mail extends React.Component {
           />
         </div>
       </div>
-    )
+    );
   }
 }
 
