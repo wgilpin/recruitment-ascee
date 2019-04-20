c96fd4beb2baa16ea5492bedcb8de39a06b11851
William Gilpin <wgilpin@gmail.com>
2019-04-20 11:17:08 +0100

    image upload in application

--- client/src/Applicant/Images.js
+++ client/src/Applicant/Images.js
@@ -1,21 +1,53 @@
 import React from 'react';
+import Loader from 'react-loader-spinner';
 import DeleteImg from '../images/baseline_delete_white_24dp.png';
+import FetchData from '../common/FetchData';
 
-const styles={
-  fadein: { 
+const styles = {
+  fadein: {
     animation: 'fadein 2s',
+  },
+};
+
+export default class Images extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      images: [],
+      loading: true,
+    }
+  }
+
+  fetchImages = () => {
+    new FetchData({scope: 'user/images'})
+      .get()
+      .then(({info}) => this.setState({ images: info, loading: false }));
+
+  }
+  componentDidMount() {
+    this.fetchImages();
+  }
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <Loader
+          type="Puff"
+          color="#01799A"
+          height="100"
+          width="100"
+        />)
+    }
+    return this.props.images.map((image, i) => (
+      <div key={i} style={styles.fadein}>
+        <div
+          onClick={() => this.props.removeImage(image.public_id)}
+          className="delete"
+        >
+          <img src={DeleteImg} alt="delete" />
+        </div>
+        <img src={image.url} alt="" />
+      </div>
+    ));
   }
 }
-
-export default props =>
-  props.images.map((image, i) => (
-    <div key={i} style={styles.fadein}>
-      <div
-        onClick={() => props.removeImage(image.public_id)}
-        className="delete"
-      >
-        <img src={DeleteImg} alt="delete" />
-      </div>
-      <img src={image.secure_url} alt="" />
-    </div>
-  ));
