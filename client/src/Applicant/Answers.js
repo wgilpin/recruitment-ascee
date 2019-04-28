7990057dbb2443d8a6ee859e0ba086038cff97ce
wgilpin <wgilpin@gmail.com>
2019-04-28 18:11:10 +0100

    revert mis-merge

--- client/src/Applicant/Answers.js
+++ client/src/Applicant/Answers.js
@@ -1,12 +1,10 @@
-import React from 'reactn';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './ApplicantStyles';
 import FetchData from '../common/FetchData';
-import Alert from './../common/Alert';
 
 const propTypes = {
-  onChange: PropTypes.func,
-  onHasApplication: PropTypes.func,
+  onReadyStatus: PropTypes.bool,
   readonly: PropTypes.bool,
   targetId: PropTypes.number,
 };
@@ -18,35 +16,27 @@ export default class Answers extends React.Component {
     super(props);
     this.state = {
       dirtyAnswers: false,
+      answers: props.answers,
     };
   }
 
-  handleAnswerChanged = e => {
-    if (this.props.readonly) {
-      return;
+  componentDidMount() {
+    this.checkAllQuestionsAnswered();
+  }
+
+  checkAllQuestionsAnswered = () => {
+    const { answers, dirtyAnswers } = this.state;
+    if (Object.keys(answers).length === 0 || dirtyAnswers) {
+      this.props.onReadyStatus(false);
     }
-    const { id, value } = e.target;
-    this.setState(
-      {
-        dirtyAnswers: true,
-      },
-      () => {
-        this.setGlobal({
-          answers: {
-            ...this.global.answers,
-            [id]: value,
-          },
-        });
-        if (this.props.onChange && !this.props.readonly) {
-          this.props.onChange();
-        }
-      }
+    this.props.onReadyStatus (
+      Object.values(answers).filter(q => q.length === 0).length === 0
     );
   };
 
   stateToParams = () => {
     return {
-      answers: Object.entries(this.global.answers).map(
+      answers: Object.entries(this.state.answers).map(
         ([question_id, text]) => ({ question_id, text })
       ),
     };
@@ -54,25 +44,39 @@ export default class Answers extends React.Component {
 
   handleSaveAnswers = () => {
     new FetchData({ scope: 'answers' }).put(this.stateToParams()).then(() => {
-      this.setState({ dirtyAnswers: false, showSavedAlert: true });
+      this.setState({ dirtyAnswers: false }, this.checkAllQuestionsAnswered);
+      window.alert('Saved');
     });
   };
 
+  handleAnswerChanged = e => {
+    const { id, value } = e.target;
+    this.setState(
+      {
+        dirtyAnswers: true,
+        answers: {
+          ...this.state.answers,
+          [id]: value,
+        },
+      },
+      this.checkAllQuestionsAnswered,
+    );
+  };
+
   render() {
+    const { questions } = this.props;
     return (
       <React.Fragment>
         <h2 style={styles.heading}>Recruitment Questions</h2>
-        {Object.keys(this.global.questions || {}).map(key => {
-          const question = this.global.questions[key];
-          const answer = this.global.answers[key];
+        {Object.keys(questions || {}).map(key => {
+          const question = questions[key];
+          const answer = this.state.answers[key];
           return (
             <div key={key}>
               <div style={styles.padded}>{question}</div>
               {this.props.readonly ? (
-                <div style={styles.answerText} id={key}>
-                  {answer.split('\n').map(line => (
-                    <div>{line}</div>
-                  ))}
+                <div style={styles.answerText} id={key} >
+                  {answer.split('\n').map(line => <div>{line}</div>)}
                 </div>
               ) : (
                 <textarea
@@ -92,12 +96,6 @@ export default class Answers extends React.Component {
           <button style={styles.primaryButton} onClick={this.handleSaveAnswers}>
             Save
           </button>
-        )}
-        {this.state.showSavedAlert && (
-          <Alert
-            text="Saved"
-            onClose={() => this.setState({ showSavedAlert: false })}
-          />
         )}
       </React.Fragment>
     );
