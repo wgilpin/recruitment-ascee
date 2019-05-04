18859582429c3fa7a1771ac117af46833283821c
wgilpin <wgilpin@gmail.com>
2019-05-04 23:13:52 +0100

    sort correctly when zeros in numeric field

--- client/src/evidence/TableBase.js
+++ client/src/evidence/TableBase.js
@@ -82,7 +82,7 @@ export default class TableBase extends React.Component {
       id,
       kind,
       header: header || this.titleise(id),
-      displayValues
+      displayValues,
     });
   }
 
@@ -200,7 +200,9 @@ export default class TableBase extends React.Component {
     if (!value) {
       return <div style={style}>{displayValues[1]}</div>;
     }
-    return <div style={style}>{!!value ? displayValues[0] : displayValues[1]}</div>;
+    return (
+      <div style={style}>{!!value ? displayValues[0] : displayValues[1]}</div>
+    );
   }
 
   makeISKField(value, field, final) {
@@ -342,7 +344,7 @@ export default class TableBase extends React.Component {
 
   sortFn(property, fieldKind) {
     const defaultVal = fieldKind === 'standing' ? 0 : 'zzzz';
-    var sortOrder = 1;
+    let sortOrder = 1;
     if (property[0] === '-') {
       sortOrder = -1;
       property = property.substr(1);
@@ -351,16 +353,12 @@ export default class TableBase extends React.Component {
       return (a, b) =>
         a[property] === b[property] ? 0 : a[property] ? -sortOrder : sortOrder;
     }
-    const fn = (a, b) => {
-      var result =
-        (a[property] || defaultVal) < (b[property] || defaultVal)
-          ? -1
-          : (a[property] || defaultVal) > (b[property] || defaultVal)
-          ? 1
-          : 0;
+    return (a, b) => {
+      const aPrime = a.hasOwnProperty(property) ? a[property] : defaultVal;
+      const bPrime = b.hasOwnProperty(property) ? b[property] : defaultVal;
+      let result = aPrime < bPrime ? -1 : (aPrime > bPrime ? 1 : 0);
       return result * sortOrder;
     };
-    return fn.bind(defaultVal);
   }
 
   sortColumn(field) {
