5d100afcab48da58d0f101c88a5404e3382aaf0c
William Gilpin <wgilpin@users.noreply.github.com>
2019-08-11 21:54:33 +0100

    Allowing config to work on heroku AND D.O. spaces

--- server/flask_app.py
+++ server/flask_app.py
@@ -4,5 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 # [START create_app]
 app = Flask(__name__, static_folder="public/static", template_folder="public")
 # [END create_app]
-app.secret_key = os.environ['APP_SECRET_KEY']
+
+if os.environ['APP_SECRET_KEY']:
+	app.secret_key = os.environ['APP_SECRET_KEY']
+else:
+	app.secret_key = b'\xc1b\xe2\xfd\xa2\xd4AG}\xfep\x9c*Fq.'
 
