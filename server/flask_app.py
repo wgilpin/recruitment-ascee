6550b794db054c727532de3746bc7e7dc209f028
William Gilpin <wgilpin@users.noreply.github.com>
2019-08-11 22:02:05 +0100

    replaced tabs with spaces
    Because iPad editor

--- server/flask_app.py
+++ server/flask_app.py
@@ -6,7 +6,7 @@ app = Flask(__name__, static_folder="public/static", template_folder="public")
 # [END create_app]
 
 if os.environ['APP_SECRET_KEY']:
-	app.secret_key = os.environ['APP_SECRET_KEY']
+    app.secret_key = os.environ['APP_SECRET_KEY']
 else:
-	app.secret_key = b'\xc1b\xe2\xfd\xa2\xd4AG}\xfep\x9c*Fq.'
+    app.secret_key = b'\xc1b\xe2\xfd\xa2\xd4AG}\xfep\x9c*Fq.'
 
